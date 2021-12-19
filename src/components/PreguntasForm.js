bfc707d8da1242e2155840c6a715ff2f315384cf
RodrigoDEVS <rodrigo.gallego@outlook.es>
2021-12-19 10:28:31 -0500

    se cargan las plantillas para los formularios

--- src/components/PreguntasForm.js
+++ src/components/PreguntasForm.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap'
 
-const preguntasForm = () => {
+const PreguntasForm = () => {
     return (
         <div>
-            
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor="TextInput">Para Iniciar Digite su Nombre</Form.Label>
+                    <Form.Control id="TextInput" placeholder="Nombre" />
+                </Form.Group>
+                <fieldset disabled>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor="TextInput">Puntaje Acumulado</Form.Label>
+                    <Form.Control id="TextInput"/>
+                </Form.Group>
+                </fieldset>
+                <Button type="submit">Guardar</Button>
+            </Form>
         </div>
     )
 }
 
-export default preguntasForm
+export default PreguntasForm
--- src/pages/Preguntas.js
+++ src/pages/Preguntas.js
@@ -1,9 +1,11 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col } from "react-bootstrap";
+import PreguntasForm from "../components/PreguntasForm";
 
 const Preguntas = () => {
     return (
         <>
             <h2>Preguntas</h2>
+            <PreguntasForm mensaje="Hola Mundo desde PreguntasForm" /><br></br>
             <Card className="text-center">
                 <Card.Body>
                     <Card.Text>
@@ -12,10 +14,16 @@ const Preguntas = () => {
                 </Card.Body>
             </Card>
             <>
-                <Button variant="primary">Primary</Button>{' '}
-                <Button variant="primary">Primary</Button>{' '}
-                <Button variant="primary">Primary</Button>{' '}
-                <Button variant="primary">Primary</Button>{' '}
+                <Col>
+                    <Button variant="primary">Primary</Button>{' '}
+                    <Button variant="primary">Primary</Button>{' '}
+                </Col>
+                <Col>
+                    <Button variant="primary">Primary</Button>{' '}
+                    <Button variant="primary">Primary</Button>{' '}
+                </Col>
+
+
             </>
         </>
     );
