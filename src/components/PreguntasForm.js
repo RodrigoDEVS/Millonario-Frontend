7b323b79b593a3d0b98a77d041fe914badd89661
RodrigoDEVS <rodrigo.gallego@outlook.es>
2021-12-19 12:07:08 -0500

    se agregan los handles para el formulario

--- src/components/PreguntasForm.js
+++ src/components/PreguntasForm.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
 const PreguntasForm = () => {
+
+    const objForm = {
+        name: ''
+    }
+
+    const [form, setForm] = useState(objForm);
+
+    const handleForm = (e)=>{
+        let obj = {...form, [e.target.name]: e.target.value};
+        setForm(obj)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log("Enviar formulario");
+        setForm(objForm)
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
-                    <Form.Label htmlFor="TextInput">Para Iniciar Digite su Nombre</Form.Label>
-                    <Form.Control id="TextInput" placeholder="Nombre" />
+                    <Form.Label htmlFor="name">Para Iniciar Ingrese su Nombre</Form.Label>
+                    <Form.Control value={form.name} onChange={handleForm} name="name" id="name" type="text" placeholder="Nombre" />
                 </Form.Group>
-                <fieldset disabled>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="TextInput">Puntaje Acumulado</Form.Label>
-                    <Form.Control id="TextInput"/>
-                </Form.Group>
-                </fieldset>
+                
                 <Button type="submit">Guardar</Button>
             </Form>
         </div>
